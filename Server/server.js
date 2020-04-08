99fb0b330c793bbde520fb500191b7bc379ecaf8
anshulsinghh <a2singh@ucsd.edu>
2020-04-08 04:08:43 -0700

    Added mpg route, which sends back a car's miles per gallon when queried.

--- Server/server.js
+++ Server/server.js
@@ -105,7 +105,7 @@ app.get("/api/v1.0/model-variations", (req, res) => {
     return
   }
   
-  let sqlQuery = `SELECT variation, epa_id FROM vehicles WHERE year=${year} AND make=${make} AND model=${model}`
+  let sqlQuery = `SELECT variation FROM vehicles WHERE year=${year} AND make=${make} AND model=${model}`
   pool.query(sqlQuery, (err, results, fields) => {
     if (err) {
       res.status(500)
@@ -123,6 +123,33 @@ app.get("/api/v1.0/model-variations", (req, res) => {
   })
 })
 
-app.get("/api/v1.0/data/:vehicleid", (req, res) => {
+app.get("/api/v1.0/mpg", (req, res) => {
+  let year = req.query.year
+  let make = req.query.make
+  let model = req.query.model
+  let variation = req.query.variation
 
+  if (typeof year !== 'string' || typeof make !== 'string' || typeof model !== 'string' || typeof variation !== 'string') {
+    res.status(400)
+    res.send('Please specify a year, make, model, and variation as string parameters.')
+    res.end()
+    return
+  }
+  
+  let sqlQuery = `SELECT * FROM vehicles WHERE year=${year} AND make=${make} AND model=${model} AND variation=${variation}`
+  pool.query(sqlQuery, (err, results, fields) => {
+    if (err) {
+      res.status(500)
+      res.send("SQL query failed.")
+      res.end()
+      return
+    }
+
+    let output = []
+    for (var i = 0; i < results.length; i++) {
+      output.push(results[i].mpg)
+    }
+
+    res.json(output)
+  })
 })
