a0750727407f6e94424550091f27381879b015f8
anshulsinghh <a2singh@ucsd.edu>
2020-05-12 00:21:30 -0700

    Fixed bug in itemselector that caused a useless constructor warning.

---
+++ Client/src/components/Selector/ItemSelector.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+
+class ItemSelector extends React.Component {
+  state = {
+    selectedItem: ""
+  }
+
+  async handleChange(event) {
+    await this.setState({selectedItem: event.target.value})
+    this.props.callback(this.state.selectedItem)
+  }
+
+  clearSelectedItem() {
+    this.setState({selectedItem: ""})
+  }
+
+  render() {
+    var items = <div></div>
+    if (!this.props.disabled && this.props.data != null) {
+      items = Object.entries(this.props.data).reverse().map(data => <MenuItem key = {data[0]} value={data[1]}>{data[1]}</MenuItem>)
+    }
+
+    return (
+      <FormControl style={this.props.styleguide} disabled={this.props.disabled}>
+        <InputLabel>{this.props.label}</InputLabel>
+        <Select
+          value={this.state.selectedItem}
+          onChange={(event) => this.handleChange(event)}
+        >
+          {items}
+        </Select>
+      </FormControl>
+    )
+  }
+}
+
+export default ItemSelector
