1d7cbf11b848b7389b32defedfec20155245d1f2
anshulsinghh <a2singh@ucsd.edu>
2020-05-13 22:34:33 -0700

    Created new datacard component

---
+++ Client/src/components/DataCard/DataCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//import { Stepper, Step, StepLabel, Button } from '@material-ui/core'
+
+class DataCard extends React.Component {
+  state = {
+
+  }
+  
+  async componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default DataCard
