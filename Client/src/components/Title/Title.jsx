76e48f8cf596e1e431636229bddf896e7021be1e
anshulsinghh <a2singh@ucsd.edu>
2020-05-14 23:10:19 -0700

    Finished title bar component

--- Client/src/components/Title/Title.jsx
+++ Client/src/components/Title/Title.jsx
@@ -5,7 +5,7 @@ import { AppBar } from '@material-ui/core'
 const Title = () => {
   return (
     <AppBar style={{'text-align': 'center', 'marginBottom': 15}} position="static" color="background">
-      <span style={{'font-family': 'Shrikhand', fontSize: 40, display: 'inline-block'}}>FuelEfficient 🍃</span>
+      <span style={{'font-family': 'Shrikhand', fontSize: 40, display: 'inline-block'}} role="img" aria-label="Falling leaves emoji">FuelEfficient 🍃</span>
     </AppBar>
   )
 }
