2b2a6446a441f16d0fcd33683e982dcbcfa39a90
anshulsinghh <a2singh@ucsd.edu>
2020-05-14 20:03:15 -0700

    Created final version of the DataCard component for visualizing the car data.

--- Client/src/components/DataCard/DataCard.jsx
+++ Client/src/components/DataCard/DataCard.jsx
@@ -1,19 +1,49 @@
 import React from 'react'
 
-//import { Stepper, Step, StepLabel, Button } from '@material-ui/core'
+import {
+  Card, CardContent, Typography
+} from '@material-ui/core'
+
 
 class DataCard extends React.Component {
-  state = {
-
-  }
-  
-  async componentDidMount() {
-    
-  }
-
   render() {
     return (
-      <div></div>
+      <Card style={{marginTop: 10, maxWidth:375, marginLeft:15}}>
+        <CardContent>
+          <Typography variant="h5" style={{marginBottom:5}}>
+            <b>{this.props.title}</b>
+          </Typography>
+
+          <Typography variant="body1" component="div">
+            The selected car releases:
+          </Typography>
+          
+          <Typography style={{marginBottom:5}} variant="body1" component="div">
+            <span aria-label="cloud emoji" role="img">💨 </span>
+            <span><b>267818.18 lbs </b>of CO<sub>2</sub></span>
+          </Typography>
+
+          <Typography variant="body1" component="div">
+            This is equivalent to:
+
+            <br/>
+
+            <span aria-label="home emoji" role="img">🏘️ </span>
+            <b>{this.props.homes} </b>Homes' monthly emissions
+
+            <br/>
+
+            <span aria-label="plane emoji" role="img">✈️ </span>
+            <b>{this.props.flights} </b>Flights from San Diego to Phoenix
+
+            <br/>
+
+            <span aria-label="home emoji" role="img">🌲 </span>
+            <b>{this.props.trees} </b>40 year-old trees
+
+          </Typography>
+        </CardContent>
+      </Card>
     )
   }
 }
