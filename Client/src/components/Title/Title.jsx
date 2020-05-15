488124641bd34ab69bc226aeb9c3bf90f5477773
anshulsinghh <a2singh@ucsd.edu>
2020-05-14 22:31:00 -0700

    Finalized title bar appearance in the webapp

--- Client/src/components/Title/Title.jsx
+++ Client/src/components/Title/Title.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import { AppBar } from '@material-ui/core'
+
 const Title = () => {
   return (
-    <h1>FuelEfficient</h1>
+    <AppBar style={{'text-align': 'center', 'marginBottom': 15}} position="static" color="background">
+      <span style={{'font-family': 'Shrikhand', fontSize: 40, display: 'inline-block'}}>FuelEfficient 🍃</span>
+    </AppBar>
   )
 }
 
