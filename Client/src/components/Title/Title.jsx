cfcb7057221e66030b8d82bb7030e5b7193336af
anshulsinghh <a2singh@ucsd.edu>
2020-05-15 17:56:46 -0700

    Changed emoji in the Title of the site

--- Client/src/components/Title/Title.jsx
+++ Client/src/components/Title/Title.jsx
@@ -5,7 +5,7 @@ import { AppBar } from '@material-ui/core'
 const Title = () => {
   return (
     <AppBar style={{'textAlign': 'center', 'marginBottom': 15}} position="static" color="inherit">
-      <span style={{'fontFamily': 'Shrikhand', fontSize: 40, display: 'inline-block'}} role="img" aria-label="Falling leaves emoji">FuelEfficient 🍃</span>
+      <span style={{'fontFamily': 'Shrikhand', fontSize: 40, display: 'inline-block'}} role="img" aria-label="Falling leaves emoji">FuelEfficient 🌿</span>
     </AppBar>
   )
 }
