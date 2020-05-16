436bec457753228607a16629cfe475145554ccd4
anshulsinghh <a2singh@ucsd.edu>
2020-05-15 17:57:15 -0700

    Added exporting support for the new DataCard component

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Title } from './Title/Title'
 export { default as Selector } from './Selector/Selector'
 export { default as DataCard } from './DataCard/DataCard'
+export { default as InfoCard } from './InfoCard/InfoCard.jsx'
