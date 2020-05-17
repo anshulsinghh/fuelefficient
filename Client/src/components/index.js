302c1b62ea48c30d0bae99d5b0eb670066c3bf54
anshulsinghh <a2singh@ucsd.edu>
2020-05-16 19:21:36 -0700

    Added support for DataCard and InfoCard's new locations into the index.js exporting file

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Title } from './Title/Title'
 export { default as Selector } from './Selector/Selector'
-export { default as DataCard } from './DataCard/DataCard'
-export { default as InfoCard } from './InfoCard/InfoCard.jsx'
+export { default as DataCard } from './Cards/DataCard'
+export { default as InfoCard } from './Cards/InfoCard'
