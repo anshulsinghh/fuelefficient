179cb76f0bf131d62be83cc5304a5d621b32856b
anshulsinghh <a2singh@ucsd.edu>
2020-05-16 19:17:35 -0700

    Added flexbox behavior for the Data and InfoCards :

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Title, Selector, DataCard, InfoCard } from './components'
 import { ThemeProvider } from "@material-ui/styles"
-import { Grid } from '@material-ui/core'
+import './Flex.css'
 
 import {
   createMuiTheme,
@@ -50,12 +50,17 @@ class App extends React.Component {
       let mpgdata15000 = this.state.mpg_data["15000 miles"]
       let mpgdata150000 = this.state.mpg_data["150000 miles"]
 
-      cards = <Grid container direction="row" justify="center" alignItems="center">
-                <InfoCard mpg={this.state.mpg_data["mpg"]}/>
-                <DataCard title="Over 100 miles..." co2={mpgdata100["CO2 emitted"]} homes={mpgdata100["Household"]} flights={mpgdata100["Flights from SD to PHX"]} trees={mpgdata100["Tree"]}/>
-                <DataCard title="Over 10,000 miles..." co2={mpgdata15000["CO2 emitted"]} homes={mpgdata15000["Household"]} flights={mpgdata15000["Flights from SD to PHX"]} trees={mpgdata15000["Tree"]}/>
-                <DataCard title="Over 150,000 miles..." co2={mpgdata150000["CO2 emitted"]} homes={mpgdata150000["Household"]} flights={mpgdata150000["Flights from SD to PHX"]} trees={mpgdata150000["Tree"]}/>
-              </Grid>
+      cards = <div class="flex-outer">
+                <div class="flex-inner">
+                  <InfoCard mpg={this.state.mpg_data["mpg"]}/>
+                  <DataCard title="Over 100 miles..." co2={mpgdata100["CO2 emitted"]} homes={mpgdata100["Household"]} flights={mpgdata100["Flights from SD to PHX"]} trees={mpgdata100["Tree"]}/>
+                </div>
+
+                <div class="flex-inner">
+                  <DataCard title="Over 10,000 miles..." co2={mpgdata15000["CO2 emitted"]} homes={mpgdata15000["Household"]} flights={mpgdata15000["Flights from SD to PHX"]} trees={mpgdata15000["Tree"]}/>
+                  <DataCard title="Over 150,000 miles..." co2={mpgdata150000["CO2 emitted"]} homes={mpgdata150000["Household"]} flights={mpgdata150000["Flights from SD to PHX"]} trees={mpgdata150000["Tree"]}/>
+                </div>
+              </div>
     }
     
     return (
@@ -65,7 +70,7 @@ class App extends React.Component {
         
 
         <Selector buttonPushed={(fueldata) => this.buttonPushed(fueldata)} selectionChanged={() => this.selectionChanged()}/>
-        
+
         {cards}
         
         
