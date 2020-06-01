94463f71abe8bb1367ab13dc8de16162d4a5d7ae
anshulsinghh <a2singh@ucsd.edu>
2020-05-31 23:29:32 -0700

    Added comments ot the Welcome component

--- Client/src/components/Welcome/Welcome.jsx
+++ Client/src/components/Welcome/Welcome.jsx
@@ -6,17 +6,20 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 class Welcome extends React.Component {
   render() {
     return (
+      // Centers the WelcomeCard in the webpage
       <div className="center-container">
+        {/* The Card container for the Welcome message*/}
         <Card className="WelcomeCard">
           <CardContent>
+            {/* The Welcome message with waving emoji */}
             <Typography variant="h4">
               <b><span aria-label="cloud emoji" role="img">👋</span> Welcome!</b>
             </Typography>
 
+            {/* The main message of the Welcome Card */}
             <Typography variant="body1" component="div">
               FuelEfficient is a place to see and understand your vehicle's emissions.
-              <br/>
-              <br/>
+              <br/><br/>
               Input your car's information in the tool below to get started!
             </Typography>
           </CardContent>
