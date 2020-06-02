254f8fd21e2b805d10dd8a0208ceb152bde614fa
anshulsinghh <a2singh@ucsd.edu>
2020-06-02 01:21:15 -0700

    Added comments for the DataCard component

--- Client/src/components/Cards/DataCard.jsx
+++ Client/src/components/Cards/DataCard.jsx
@@ -1,48 +1,47 @@
 import React from 'react'
+import './CardStyles.css'
 
 import { Card, CardContent, Typography } from '@material-ui/core'
 
-class DataCard extends React.Component {
-  render() {
-    return (
-      <Card className="ContentCard">
-        <CardContent>
-          <Typography variant="h5" style={{marginBottom:5}}>
-            <b>{this.props.title}</b>
-          </Typography>
+function DataCard(props) {
+  return (
+    <Card className="ContentCard">
+      <CardContent>
+        <Typography variant="h5" className="spacer">
+          <b>{props.title}</b>
+        </Typography>
 
-          <Typography variant="body1" component="div">
-            The selected car releases:
-          </Typography>
-          
-          <Typography style={{marginBottom:5}} variant="body1" component="div">
-            <span aria-label="cloud emoji" role="img">💨 </span>
-            <span><b>{this.props.co2} lbs </b>of CO<sub>2</sub></span>
-          </Typography>
+        <Typography variant="body1" component="div">
+          The selected car releases:
+        </Typography>
+        
+        <Typography className="spacer" variant="body1" component="div">
+          <span aria-label="cloud emoji" role="img">💨 </span>
+          <span><b>{props.co2} lbs</b> of CO<sub>2</sub></span>
+        </Typography>
 
-          <Typography variant="body1" component="div">
-            This is equivalent to:
+        <Typography variant="body1" component="div">
+          This is equivalent to:
 
-            <br/>
+          <br/>
 
-            <span aria-label="home emoji" role="img">🏘️ </span>
-            <b>{this.props.homes} </b>Homes' monthly emissions
+          <span aria-label="home emoji" role="img">🏘️ </span>
+          <b>{props.homes}</b> Homes' monthly emissions
 
-            <br/>
+          <br/>
 
-            <span aria-label="plane emoji" role="img">✈️ </span>
-            <b>{this.props.flights} </b>Flights from San Diego to Phoenix
+          <span aria-label="plane emoji" role="img">✈️ </span>
+          <b>{props.flights}</b> Flights from San Diego to Phoenix
 
-            <br/>
+          <br/>
 
-            <span aria-label="home emoji" role="img">🌲 </span>
-            <b>{this.props.trees} </b>40 year-old trees
+          <span aria-label="home emoji" role="img">🌲 </span>
+          <b>{props.trees}</b> 40 year-old trees
 
-          </Typography>
-        </CardContent>
-      </Card>
-    )
-  }
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default DataCard
