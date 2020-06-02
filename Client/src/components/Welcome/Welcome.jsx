fd5edc933faee4ff855b741f20b96e7f6ac5aa92
anshulsinghh <a2singh@ucsd.edu>
2020-06-02 01:57:50 -0700

    Changed Welcome componet to a const component

--- Client/src/components/Welcome/Welcome.jsx
+++ Client/src/components/Welcome/Welcome.jsx
@@ -3,30 +3,28 @@ import './Welcome.css'
 
 import { Card, CardContent, Typography } from '@material-ui/core'
 
-class Welcome extends React.Component {
-  render() {
-    return (
-      // Centers the WelcomeCard in the webpage
-      <div className="center-container">
-        {/* The Card container for the Welcome message*/}
-        <Card className="WelcomeCard">
-          <CardContent>
-            {/* The Welcome message with waving emoji */}
-            <Typography variant="h4">
-              <b><span aria-label="cloud emoji" role="img">👋</span> Welcome!</b>
-            </Typography>
+const Welcome = () => {
+  return (
+    // Centers the WelcomeCard in the webpage
+    <div className="center-container">
+      {/* The Card container for the Welcome message*/}
+      <Card className="WelcomeCard">
+        <CardContent>
+          {/* The Welcome message with waving emoji */}
+          <Typography variant="h4">
+            <b><span aria-label="cloud emoji" role="img">👋</span> Welcome!</b>
+          </Typography>
 
-            {/* The main message of the Welcome Card */}
-            <Typography variant="body1" component="div">
-              FuelEfficient is a place to see and understand your vehicle's emissions.
-              <br/><br/>
-              Input your car's information in the tool below to get started!
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-    )
-  }
+          {/* The main message of the Welcome Card */}
+          <Typography variant="body1" component="div">
+            FuelEfficient is a place to see and understand your vehicle's emissions.
+            <br/><br/>
+            Input your car's information in the tool below to get started!
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
 
 export default Welcome
