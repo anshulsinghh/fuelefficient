43441628ce0515f05120d6f91df41ea8af867dde
anshulsinghh <a2singh@ucsd.edu>
2020-06-09 04:09:03 -0700

    Configured server.js to add routes for build-ready client in production

--- Server/server.js
+++ Server/server.js
@@ -10,13 +10,20 @@ app.use(cors())
 app.use(require('./Routes'))
 
 const pool = mysql.createPool({
-  host:'test-db-2.crpalwy0pful.us-west-1.rds.amazonaws.com',
-  user:'root',
-  password:'***REMOVED***',
-  database:'fuelefficient-data'
+  host: 'test-db-2.crpalwy0pful.us-west-1.rds.amazonaws.com',
+  user: 'root',
+  password: '***REMOVED***',
+  database: 'fuelefficient-data'
 })
 
 module.exports = pool
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('Client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html'))
+  })
+}
 
 //Start the API on port 4000
 app.listen(4000, () => {
