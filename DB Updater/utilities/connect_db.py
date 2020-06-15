569aca7b7cd1e2db449b50479af58ae13e384805
anshulsinghh <a2singh@ucsd.edu>
2020-06-14 17:10:09 -0700

    Renamed DB_Updater to DB Updater, removed DB connection details

--- DB Updater/utilities/connect_db.py
+++ DB Updater/utilities/connect_db.py
@@ -7,8 +7,8 @@ import mysql.connector
 
 def connect_db():
     return mysql.connector.connect(
-        host="test-db-2.crpalwy0pful.us-west-1.rds.amazonaws.com",
-        user="root",
-        passwd="***REMOVED***",
-        database="fuelefficient-data"
+        host="",
+        user="",
+        passwd="",
+        database=""
     )
